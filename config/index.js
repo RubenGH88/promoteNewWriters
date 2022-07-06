6261a9346de359228b66662875e04390e16edd40
RubenGH88 <104738166+RubenGH88@users.noreply.github.com>
2022-07-06 23:07:44 +0200

    project2
    delete files ok

--- config/index.js
+++ config/index.js
@@ -10,7 +10,6 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
 
-
 // ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
 // https://www.npmjs.com/package/cookie-parser
 const cookieParser = require("cookie-parser");
@@ -45,6 +44,7 @@ module.exports = (app) => {
   app.use(cookieParser());
   app.use(cors());
   app.use(fileUpload({createParentPath:true}));
+  
   
 
   
