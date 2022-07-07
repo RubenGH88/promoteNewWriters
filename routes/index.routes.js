23caac69e0de5d9540b3723e19628eed51ab6322
RubenGH88 <104738166+RubenGH88@users.noreply.github.com>
2022-07-08 01:00:01 +0200

    all links corrected
    corrected links

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  let logged=false
+
+        if(req.session.user){logged=true}
+    
+  res.render("index",{logged});
 });
 
 module.exports = router;
